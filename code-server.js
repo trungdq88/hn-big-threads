624c03fc999d8cc181b912c7bdf70cd60f57b4e8
Tony Dinh <gh@tdinh.me>
2021-01-10 18:58:41 +0700

    Init

---
+++ code-server.js
@@ -0,0 +1,48 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+async function get(id) {
+  return await fetch(
+    `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+  ).then(r => r.json());
+}
+
+async function pull(id) {
+  console.log('pulling', id);
+  const node = await get(id);
+
+  const children = [];
+  let value = 1;
+
+  const pulledKids = await Promise.all(
+    ((node && node.kids) || []).map(k => pull(k))
+  );
+
+  let data = {};
+
+  for (k of pulledKids) {
+    // k.parent = data;
+    children.push(k);
+    value += k.value;
+  }
+
+  data.id = id;
+  data.name = node ? node.by : '?';
+  data.value = value;
+  data.text = node ? node.text || node.title || node.url : '?';
+  data.children = children;
+
+  if (children.length === 0) {
+    delete data.children;
+  }
+
+  return data;
+}
+
+async function start(id) {
+  const data = await pull(id);
+
+  fs.writeFileSync('data.json', JSON.stringify(data), 'utf-8');
+}
+
+start(25706993);
